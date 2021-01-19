9ddffaaa8a7541e8cda7f963c2e6d7321ae3fe33
Sarguhl <52204356+sarguhl1337@users.noreply.github.com>
2021-01-19 13:16:26 +0100

    Lession 1.5, Summary

---
+++ Lession1/5_summary.py
@@ -0,0 +1,23 @@
+"""
+In Python, as in many other programming languages, there are planty of different data types.
+
+But I'd consider the most important ones for beginning:
+- String
+- Integer
+- Float
+- And boolean
+
+Please feel free, to experiment around with them!
+
+Problems you might will run into:
+- When trying to add an integer and string:
+    print("Hello" + 1)
+    this will give you an error, because Python won't let you add
+    strings and integers together. To "glue" them together, you can
+    use the following method:
+    To convert an integer into a string do this:
+        print("Hello" + str(1))
+    
+    To convert a string into an interger do this:
+        print(int("1") + 5)
+"""
