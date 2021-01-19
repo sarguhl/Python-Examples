9a9f32339f99139a26b0ea6584063bbb483ce203
Sarguhl <52204356+sarguhl1337@users.noreply.github.com>
2021-01-19 13:05:06 +0100

    Lession 1.4, Booleans

---
+++ Lession1/4_booleans.py
@@ -0,0 +1,25 @@
+# A boolean is also a data type. But the difference is, that is can only be True or False.
+# For this, let me introduce a new operator. It's calles the "eqal" operator.
+# It's being used, to compare two items and look, if they're the same.
+# For example: 1 == 2, 4 == 4, "Hello" == "Hello", ...
+
+#Let's try it out!
+print(1 == 1) # 1 is 1, so it's True.
+
+print(1 == 2) # 1 is not 2, so it's Fale. 
+
+# You can compare every data type. For example strings:
+print("Hello" == "Hello")
+
+print("Hello" == "World")
+
+# There is also another operator, that can be used for booleans. It's the "not equal" operator.
+# It works like the equal operator, just backwards.
+# For example: 1 != 1, 4 != 4, "Hello" != "Hello", ...
+
+# Let's try this one out.
+print(1 != 1) # 1 is 1, so it's Fale.
+
+print(1 != 2) # 1 is not 2, so it's True.
+
+# As with the equal operator, this works for every data type.
