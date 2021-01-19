891e54cb726e45fb66feb907c795013ab72183a0
Sarguhl <52204356+sarguhl1337@users.noreply.github.com>
2021-01-19 12:45:44 +0100

    Lesson 1.1, Strings

---
+++ Lession1/strings.py
@@ -0,0 +1,17 @@
+# A string is an data type that let's you handle text. It's mostly represented with text,
+# between double quotes ("").
+
+# Here, we are printing the text "Hello World" out to the screen. To do that, we are using the -print- function.
+print("Hello World!")
+
+# About strings:
+# A string, is declared in the following ways:
+
+"Hello" # With double quotes
+
+'Hello' # With single quotes
+
+"""
+Hello,
+World.
+""" # Over multiple lines.
